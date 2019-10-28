6a614008c37d134bbbfc6cea9b3a1d82af9cf675
Fraz Ahsan <fraz.ahsan17@gmail.com>
2019-10-28 20:33:11 +0500

    Sidekiq cron added

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,9 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://127.0.0.1:6379/0' }
 end
+
+schedule_file = "config/schedule.yml"
+
+if File.exists?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
