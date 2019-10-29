82902c3ec7f7b2bd41e3d87f90475d6b93e941d0
Fraz Ahsan <fraz.ahsan17@gmail.com>
2019-10-29 18:17:18 +0500

    Slack response added for triggger

--- app/controllers/upwork_controller.rb
+++ app/controllers/upwork_controller.rb
@@ -1,7 +1,11 @@
 class UpworkController < ApplicationController
   def suspend
+    #TODO call workers
+    render status: 200, json: {"text": "Sending Request to Upwork. Please Wait....", "replace_original": "true"}
   end
 
   def restart
+    #TODO call workers here
+    render status: 200, json: {"text": "Sending Request to Upwork. Please Wait....", "replace_original": "true"}
   end
 end
